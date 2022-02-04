44b5731a3b442058788de6f1db3e10ddfd1e4493
Julius Florstedt <juliusflorstedt@gmail.com>
2022-02-04 16:51:26 +0100

    Structure of 2.1

---
+++ src/main/java/projekt/food/IceCream.java
@@ -0,0 +1,9 @@
+package projekt.food;
+
+public interface IceCream extends Food{
+
+    /**
+     * @return the flavor of the specific ice cream, depends on the specific implementation
+     */
+    String getFlavor();
+}

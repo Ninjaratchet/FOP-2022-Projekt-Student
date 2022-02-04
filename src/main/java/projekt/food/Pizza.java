1beba5713cc2a4ad09c92d25fefd4127293bec9f
Julius Florstedt <juliusflorstedt@gmail.com>
2022-02-04 16:42:28 +0100

    Structure of 2.1

---
+++ src/main/java/projekt/food/Pizza.java
@@ -0,0 +1,9 @@
+package projekt.food;
+
+public interface Pizza extends Saucable{
+
+    /**
+     * @return the diameter of the pizza
+     */
+    double getDiameter();
+}
---
+++ src/main/java/projekt/food/Saucable.java
@@ -0,0 +1,10 @@
+package projekt.food;
+
+public interface Saucable extends Food{
+
+    /**
+     * @return the kind of sauce served with a dish
+     */
+    String getSauce();
+
+}
